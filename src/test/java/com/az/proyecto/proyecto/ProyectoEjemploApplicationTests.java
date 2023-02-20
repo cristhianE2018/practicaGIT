27a9bbabb5dd0f7633ef94a09b6adfb7b0117aca
crisOlivares <cristhian@olivares.com>
2023-02-19 22:52:45 -0600

    initial commit

---
+++ src/test/java/com/az/proyecto/proyecto/ProyectoEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.proyecto.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
